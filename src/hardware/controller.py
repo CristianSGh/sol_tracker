4b56e7d7201d524198002efe1d1b826257b6515a
CristianSGh <cristian.ghita.cs@gmail.com>
2020-06-15 20:44:54 +0300

    add functions to read the pin values

--- src/hardware/controller.py
+++ src/hardware/controller.py
@@ -1,6 +1,10 @@
 import pifacedigitalio as pfio
 
 
+_boards_ = init_boards()
+# _board_ = pfio.PiFaceDigital()  # a single board (hardware_addr=0)
+
+
 def init_boards(num=1):
     boards = [pfio.PiFaceDigital()]  # the first board
     if num > 1:
@@ -12,12 +16,22 @@ def init_boards(num=1):
     return boards
 
 
-_boards_ = init_boards()
-# _board_ = pfio.PiFaceDigital()  # a single board (hardware_addr=0)
-
-
 def toggle_relay(relay=0, board=0):
     _boards_[board].relays[relay].toggle()
+
+
+def get_relay_status(relay=0, board=0):
+    return _boards_[board].relays[relay].value
+
+
+def get_input_status(board=0):
+    values = [_boards_[board].input_pins[x].value for x in range(8)]
+    return values
+
+
+def get_output_status(board=0):
+    values = [_boards_[board].output_pins[x].value for x in range(8)]
+    return values
 
 
 if __name__ == "__main__":
